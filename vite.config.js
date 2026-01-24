bcbb150403fed8b14061d2b28a5f01eaf9043007
Tian Lin <AstronautTL@163.com>
2026-01-25 01:13:29 +0800

    fix: update base path to match repo name lins-website

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/lin-website/',
+  base: '/lins-website/',
 })
