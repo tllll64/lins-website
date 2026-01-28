10bb6c8286be601d9bd4134b9282df3df05a6676
Tian Lin <AstronautTL@163.com>
2026-01-28 13:19:12 +0800

    优化支小宝在前端的呈现

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  base: '/lins-website/',
-})
+  base: mode === 'production' ? '/lins-website/' : '/',
+}))
