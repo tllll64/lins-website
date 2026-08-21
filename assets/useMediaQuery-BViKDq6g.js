0620e8176acb853de698cd2c631e594c652413b0
lynnltian <lynnltian@tencent.com>
2026-08-21 17:06:45 +0800

    Updates

---
+++ assets/useMediaQuery-BViKDq6g.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-B9_uBYoP.js";const o=s=>{const[t,a]=n.useState(!1);return n.useEffect(()=>{const e=window.matchMedia(s);e.matches!==t&&a(e.matches);const c=()=>a(e.matches);return e.addEventListener("change",c),()=>e.removeEventListener("change",c)},[t,s]),t};export{o as u};
